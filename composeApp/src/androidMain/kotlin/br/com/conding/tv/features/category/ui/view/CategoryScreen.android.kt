4e0cc91bfcebe0c8a52b2e3768e60d203179bbae
Eduardo Barreiros <edubarrlima@gmail.com>
2025-05-31 01:26:08 -0300

    conflicts adjustments

--- composeApp/src/androidMain/kotlin/br/com/conding/tv/features/category/ui/view/CategoryScreen.android.kt
+++ composeApp/src/androidMain/kotlin/br/com/conding/tv/features/category/ui/view/CategoryScreen.android.kt
@@ -1,14 +1,8 @@
 package br.com.conding.tv.features.category.ui.view
 
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
-import br.com.conding.tv.components.model.DefinitionsScreen
-import br.com.conding.tv.components.settings.TypeSystem
-import br.com.conding.tv.components.ui.ContentScreen
-import br.com.conding.tv.components.ui.HeaderSearch
 import br.com.conding.tv.navigation.AppDestinations
 import br.com.conding.tv.networking.resources.AlternativesRoutes
-import br.com.conding.tv.resources.GenericsStrings
 
 @Composable
 actual fun CategoriesScreen(
@@ -17,23 +11,4 @@ actual fun CategoriesScreen(
     goToAlternativeRoutes: (AlternativesRoutes?) -> Unit
 ) {
     MobileCategoryScreen(goToBackScreen, goToNextScreen, goToAlternativeRoutes)
-    ContentScreen(
-        definitionsScreen = DefinitionsScreen(
-            alignment = Alignment.Top
-        ),
-        content = {
-            HeaderSearch(
-                label = GenericsStrings.CATEGORIES,
-                system = TypeSystem.MOBILE,
-                onSearch = { name, size, sort, route ->
-                },
-                onSort = { name, size, sort, route ->
-                },
-                onFilter = { name, size, sort, route ->
-                },
-                onRefresh = { name, size, sort, route ->
-                }
-            )
-        }
-    )
 }
--- composeApp/src/androidMain/kotlin/br/com/conding/tv/features/product/ui/view/ProductScreen.android.kt
+++ composeApp/src/androidMain/kotlin/br/com/conding/tv/features/product/ui/view/ProductScreen.android.kt
@@ -1,14 +1,8 @@
 package br.com.conding.tv.features.product.ui.view
 
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
-import br.com.conding.tv.components.model.DefinitionsScreen
-import br.com.conding.tv.components.settings.TypeSystem
-import br.com.conding.tv.components.ui.ContentScreen
-import br.com.conding.tv.components.ui.HeaderSearch
 import br.com.conding.tv.navigation.AppDestinations
 import br.com.conding.tv.networking.resources.AlternativesRoutes
-import br.com.conding.tv.resources.GenericsStrings
 
 @Composable
 actual fun ProductsScreen(
@@ -17,23 +11,4 @@ actual fun ProductsScreen(
     goToAlternativeRoutes: (AlternativesRoutes?) -> Unit
 ) {
     MobileProductsScreen(goToBackScreen, goToNextScreen, goToAlternativeRoutes)
-    ContentScreen(
-        definitionsScreen = DefinitionsScreen(
-            alignment = Alignment.Top
-        ),
-        content = {
-            HeaderSearch(
-                label = GenericsStrings.PRODUCTS,
-                system = TypeSystem.MOBILE,
-                onSearch = { name, size, sort, route ->
-                },
-                onSort = { name, size, sort, route ->
-                },
-                onFilter = { name, size, sort, route ->
-                },
-                onRefresh = { name, size, sort, route ->
-                }
-            )
-        }
-    )
 }
