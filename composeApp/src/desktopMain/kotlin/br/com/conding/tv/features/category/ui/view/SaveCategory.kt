50201017052e162f27f01d053f4fc3ba7dd4f003
Flávio Júnior <59364674+flavio-junior@users.noreply.github.com>
2025-06-08 18:15:46 -0300

    feat: add action ImeAction.Go

--- composeApp/src/desktopMain/kotlin/br/com/conding/tv/features/category/ui/view/SaveCategory.kt
+++ composeApp/src/desktopMain/kotlin/br/com/conding/tv/features/category/ui/view/SaveCategory.kt
@@ -12,6 +12,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.KeyboardType
 import br.com.conding.tv.components.ui.IsErrorMessage
 import br.com.conding.tv.components.ui.LoadingButton
@@ -63,6 +64,7 @@ fun SaveCategory(
                 value = categoryName,
                 iconName = IconName.EDIT,
                 keyboardType = KeyboardType.Text,
+                imeAction = ImeAction.Go,
                 isError = observer.second,
                 onValueChange = { categoryName = it },
                 modifier = modifier.weight(weight = WEIGHT_SIZE_2),
--- composeApp/src/desktopMain/kotlin/br/com/conding/tv/features/category/ui/view/UpdateCategory.kt
+++ composeApp/src/desktopMain/kotlin/br/com/conding/tv/features/category/ui/view/UpdateCategory.kt
@@ -14,6 +14,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.KeyboardType
 import br.com.conding.tv.components.ui.Alert
 import br.com.conding.tv.components.ui.IconDefault
@@ -102,6 +103,7 @@ fun UpdateCategory(
             value = categoryName,
             iconName = IconName.EDIT,
             keyboardType = KeyboardType.Text,
+            imeAction = ImeAction.Go,
             isError = observer.second,
             message = observer.third ?: EMPTY_TEXT,
             onValueChange = {
