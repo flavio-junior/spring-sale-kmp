0e935723a99fd27c59bf63a27dfef836526f5a5c
Flávio Júnior <59364674+flavio-junior@users.noreply.github.com>
2025-06-08 19:08:10 -0300

    feat: add logo in mobile

--- composeApp/src/commonMain/kotlin/br/com/conding/tv/features/splash/SplashScreen.kt
+++ composeApp/src/commonMain/kotlin/br/com/conding/tv/features/splash/SplashScreen.kt
@@ -1,17 +1,19 @@
 package br.com.conding.tv.features.splash
 
+import androidx.compose.foundation.Image
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import br.com.conding.tv.components.ui.ContentScreen
-import br.com.conding.tv.components.ui.IconDefault
 import br.com.conding.tv.components.ui.Title
 import br.com.conding.tv.features.account.ui.view.UiResponseGetTokenSaved
 import br.com.conding.tv.features.account.ui.viewmodel.AccountViewModel
 import br.com.conding.tv.resources.GenericsStrings
-import br.com.conding.tv.resources.IconName
 import br.com.conding.tv.theme.Themes
 import kotlinx.coroutines.delay
+import org.jetbrains.compose.resources.painterResource
 import org.koin.mp.KoinPlatform.getKoin
+import springsale.composeapp.generated.resources.Res
+import springsale.composeapp.generated.resources.coding_tv
 
 @Composable
 expect fun SplashScreen(
@@ -31,11 +33,14 @@ fun MobileSplashScreen(
                 delay(timeMillis = 5000)
                 viewModel.getToken()
             }
-            IconDefault(
-                iconName = IconName.CAMPAIGN,
-                size = Themes.size.spaceSize100
+            Image(
+                painter = painterResource(resource = Res.drawable.coding_tv),
+                contentDescription = null
             )
-            Title(label = GenericsStrings.PROJECT_NAME)
+            Title(
+                label = GenericsStrings.PROJECT_NAME,
+                color = Themes.colors.primary
+            )
             UiResponseGetTokenSaved(
                 viewModel = viewModel,
                 goToSignInScreen = goToSignInScreen,
